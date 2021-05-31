b786ade7270b0aa5d1548375be91193984d33f03
prahladinala <prahlad2018@gmail.com>
2021-05-31 20:49:56 +0530

    Dynamic URL and Content from one object

--- challenges/views.py
+++ challenges/views.py
@@ -3,17 +3,27 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+monthly_challenges = {
+    'january': 'January Tasks Todo',
+    'february': 'february Tasks <br> Todo',
+    'march': 'march Tasks <br> Todo',
+    'april': 'april Tasks <br> Todo',
+    'may': 'May Tasks <br> Todo',
+    'june': 'june Tasks <br> Todo',
+    'july': 'july Tasks <br> Todo',
+    'august': 'august Tasks <br> Todo',
+    'september': 'september Tasks <br> Todo',
+    'october': 'october Tasks <br> Todo',
+    'november': 'november Tasks <br> Todo',
+    'december': 'december Tasks <br> Todo',
+}
+
+
+def monthly_challenge_by_number(request, month):
+    month = list(monthly_challenges.keys())
+    return HttpResponse(month)
+
 
 def monthly_challenge(request, month):
-    challenges_text = None
-    if month == 'january':
-        challenges_text = "January Challenges List Here"
-    elif month == 'febrauary':
-        challenges_text = "Febrauary Challenges List Here"
-    elif month == 'march':
-        challenges_text = "March Challenges List Here"
-    else:
-        return HttpResponseNotFound("IS This the name of the month ?")
+    challenges_text = monthly_challenges[month]
     return HttpResponse(challenges_text)
-def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
